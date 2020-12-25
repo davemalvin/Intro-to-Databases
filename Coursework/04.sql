0d78135ce52088eae06f441cdc5e236c84f6256f
Dave Malvin Limanda <47667667+davemalvin@users.noreply.github.com>
2020-12-26 01:19:22 +0700

    Update 04.sql

--- Coursework/04.sql
+++ Coursework/04.sql
@@ -1,1 +1,8 @@
-SELECT DISTINCT E.student FROM Exams E, (SELECT E.student, COUNT(E.grade) AS fail FROM Exams E WHERE E.grade < 40 GROUP BY E.student) AS F WHERE E.student = F.student GROUP BY E.student, F.fail HAVING (CAST(F.fail AS NUMERIC) / COUNT(E.grade)) > 0.3;
+SELECT DISTINCT E.student 
+FROM Exams E, (SELECT E.student, COUNT(E.grade) AS fail 
+               FROM Exams E 
+               WHERE E.grade < 40 
+               GROUP BY E.student) AS F 
+WHERE E.student = F.student 
+GROUP BY E.student, F.fail 
+HAVING (CAST(F.fail AS NUMERIC) / COUNT(E.grade)) > 0.3;
