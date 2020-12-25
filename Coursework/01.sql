feb3deeacba4a47198d9745ea50b491d963e0c29
Dave Malvin Limanda <47667667+davemalvin@users.noreply.github.com>
2020-12-26 01:20:19 +0700

    Update 01.sql

--- Coursework/01.sql
+++ Coursework/01.sql
@@ -1,1 +1,5 @@
-SELECT S.uun FROM Students S WHERE NOT EXISTS (SELECT DISTINCT E.student FROM Exams E WHERE S.uun = E.student);
+SELECT S.uun 
+FROM Students S 
+WHERE NOT EXISTS (SELECT DISTINCT E.student 
+                  FROM Exams E 
+                  WHERE S.uun = E.student);
