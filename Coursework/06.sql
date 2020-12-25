5a1d206e59087defda763c758f8455a4e90ecaa0
Dave Malvin Limanda <47667667+davemalvin@users.noreply.github.com>
2020-12-26 01:17:44 +0700

    Update 06.sql

--- Coursework/06.sql
+++ Coursework/06.sql
@@ -1,1 +1,14 @@
-SELECT E.student, SUM(CASE WHEN E.grade >= 80 THEN 1 ELSE 0 END) AS a, SUM(CASE WHEN E.grade >= 60 AND E.grade <= 79 THEN 1 ELSE 0 END) AS b, SUM(CASE WHEN E.grade >= 40 AND E.grade <= 59 THEN 1 ELSE 0 END) AS c, SUM(CASE WHEN E.grade < 40 THEN 1 ELSE 0 END) AS d FROM Exams E GROUP BY E.student;
+SELECT E.student, SUM(CASE WHEN E.grade >= 80 
+                      THEN 1 ELSE 0 
+                      END) AS a, 
+                   SUM(CASE WHEN E.grade >= 60 AND E.grade <= 79 
+                       THEN 1 ELSE 0 
+                       END) AS b, 
+                   SUM(CASE WHEN E.grade >= 40 AND E.grade <= 59 
+                       THEN 1 ELSE 0 
+                       END) AS c, 
+                   SUM(CASE WHEN E.grade < 40 
+                       THEN 1 ELSE 0 
+                       END) AS d 
+FROM Exams E 
+GROUP BY E.student;
