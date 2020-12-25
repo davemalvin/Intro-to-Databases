33ad9c245be38c19ff3bc893e640f4546b39cf72
Dave Malvin Limanda <47667667+davemalvin@users.noreply.github.com>
2020-12-26 01:13:18 +0700

    Update 10.sql

--- Coursework/10.sql
+++ Coursework/10.sql
@@ -1,1 +1,11 @@
-SELECT S.uun, S.name FROM Students S EXCEPT SELECT S1.uun, S1.name FROM (SELECT S.uun, S.name, P.course FROM Programmes P, Students S WHERE P.degree = S.degree EXCEPT SELECT S.uun, S.name, E.course FROM Students S, Exams E WHERE S.uun = E.student) AS S1;
+SELECT S.uun, S.name 
+FROM Students S 
+EXCEPT 
+SELECT S1.uun, S1.name 
+FROM (SELECT S.uun, S.name, P.course 
+      FROM Programmes P, Students S 
+      WHERE P.degree = S.degree 
+      EXCEPT 
+      SELECT S.uun, S.name, E.course 
+      FROM Students S, Exams E 
+      WHERE S.uun = E.student) AS S1;
