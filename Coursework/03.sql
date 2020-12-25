25c5d5d5c2140b331e8486a7e6bd5e493be88759
Dave Malvin Limanda <47667667+davemalvin@users.noreply.github.com>
2020-12-26 01:19:41 +0700

    Update 03.sql

--- Coursework/03.sql
+++ Coursework/03.sql
@@ -1,1 +1,4 @@
-SELECT E.student, MIN(E.grade) AS mingrade, MAX(E.grade) AS maxgrade, COUNT(E.grade) AS totalexams FROM Exams E GROUP BY E.student HAVING AVG(E.grade) >= 75;
+SELECT E.student, MIN(E.grade) AS mingrade, MAX(E.grade) AS maxgrade, COUNT(E.grade) AS totalexams 
+FROM Exams E 
+GROUP BY E.student 
+HAVING AVG(E.grade) >= 75;
