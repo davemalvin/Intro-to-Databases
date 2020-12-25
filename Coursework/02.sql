2948c5b6b868e50db0d0e8591aed99a97656e685
Dave Malvin Limanda <47667667+davemalvin@users.noreply.github.com>
2020-12-26 01:19:56 +0700

    Update 02.sql

--- Coursework/02.sql
+++ Coursework/02.sql
@@ -1,1 +1,3 @@
-SELECT COUNT(S.uun) AS total FROM Students S, Degrees D WHERE S.degree = D.code AND D.type = 'PG';
+SELECT COUNT(S.uun) AS total 
+FROM Students S, Degrees D 
+WHERE S.degree = D.code AND D.type = 'PG';
